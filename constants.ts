7edcc4294da4c9fa7c043456cde87f44c7acdea2
Dagim Yohannes <68786330+Dagiyoyo2020@users.noreply.github.com>
2026-01-08 17:02:43 +0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@
 
 export const SUPABASE_URL = "https://utdgtmvdwyzzoaurugmu.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0ZGd0bXZkd3l6em9hdXJ1Z211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjU5MzksImV4cCI6MjA3OTc0MTkzOX0.mQsyTJNnztdTtSeRh9tgEtIJtkYjD2837JsU9udFAQw";
-export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY || "";
+export const GEMINI_API_KEY = import.meta.env.GEMINI_API_KEY || "";
 
 export enum AppRoutes {
   HOME = "/",
