f56443b985c7e4f9c2250153c3b6f01889d44c8f
Dagim Yohannes <68786330+Dagiyoyo2020@users.noreply.github.com>
2026-01-08 13:37:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        allowedHosts: true,
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      // VITE_ prefixed variables are automatically exposed via import.meta.env
-      // No need to define them manually
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  server: {
+    allowedHosts: true,
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('.', import.meta.url)),
+    }
+  }
 });
